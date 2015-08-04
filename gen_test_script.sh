871d2966c2ba861d0c355a0a3be2993cf89a128c
Brian Quistorff <bquistorff@gmail.com>
2015-08-04 19:59:49 -0400

    small update to test script

--- gen_test_script.sh
+++ gen_test_script.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # Creates do file to test that all the packages can be -net describe-'ed.
-find | grep .pkg | sed -e 's|^\./\(.\)/\([^\.]\+\).pkg|net describe \2, from (https://raw.github.com/bquistorff/Stata-modules/master/\1/)|g' > test.do
+find | grep .pkg | sed -e 's|^\./\(.\)/\([^\.]\+\).pkg|net describe \2, from (https://raw.github.com/bquistorff/Stata-modules/master/\1/)|g' > test2.do
---
+++ test.do
@@ -0,0 +1,5 @@
+sysdir set PERSONAL "."
+sysdir set PLUS "." //some commands think this has to be in S_ADO
+global S_ADO "PERSONAL;BASE"
+net set ado PERSONAL
+mata: mata mlib index
